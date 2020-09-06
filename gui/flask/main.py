379a1963feade68581e726ec81afc8fd24bca5c1
root <root@DESKTOP-7DFHU1G.localdomain>
2020-09-06 14:55:32 +0800

    add is_json_file() function

--- gui/flask/main.py
+++ gui/flask/main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, redirect, url_for
 from werkzeug.utils import secure_filename
 import os
 import subprocess
+import json
 
 app = Flask(__name__)
 
@@ -17,16 +18,24 @@ def upload_json_file():
         json_file_save_path = './uploads/' + secure_filename(f.filename)
         f.save(json_file_save_path)
         codegen_file_path = './codegen-1.0-SNAPSHOT.jar'
-        flink_address = './'
-        cmd_str = 'java -jar' + ' ' + codegen_file_path + ' ' + json_file_save_path + ' ' + flink_address
+        generated_jar_path = './generated_jar'
+        cmd_str = 'java -jar' + ' ' + codegen_file_path + ' ' + json_file_save_path + ' ' + generated_jar_path
 
         output = subprocess.check_output(cmd_str, shell=True)
-        print(output)
+        # print(output)
         # os.system(cmd_str)
 
-        return render_template('upload.html', result = output)
+        return render_template('upload.html', result = str(output))
         # return redirect(url_for('upload_json_file'))
-    return render_template('upload.html')
+    return render_template('upload.html', result = "null")
+
+
+def is_json_file(the_file):
+    try:
+        json_obj = json.loads(the_file)
+    except ValueError, e:
+        return False
+    return True
 
 
 if __name__ == '__main__':
