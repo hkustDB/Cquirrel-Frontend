c97f8582a9d0fbc8acf3a133e7a7c6cad447e580
Dan <danishibrahim1994@hotmail.com>
2020-09-07 22:57:45 +0800

    GCG-10 made isLast final and initialized in constructor

--- codegen/JsonUtils/src/main/java/Node.java
+++ codegen/JsonUtils/src/main/java/Node.java
@@ -4,36 +4,12 @@ import java.util.List;
 import java.util.Map;
 
 public class Node {
-    /**
-     * A class representing the metaData provided by the user through the GUI in JSON format, must never be initialized manually
-     * Note: this class made on the assumption that we will use Google Gson to populate this class automatically
-     * node_name
-     * |
-     * |----relation
-     * |
-     * |----distribute key of this node
-     * |
-     * |----distribute key of next node
-     * |
-     * |----The number of child nodes
-     * |
-     * |----If the node is the root node
-     * |
-     * |----If the node is the leaf node
-     * |
-     * |----If the node is the last node of the relation
-     * |
-     * |----If any attributes need to be renamed
-     * |
-     * |----The select conditions
-     */
-
     private final String nodeName;
     private final String relationName;
     private final String distributedKey;
     private final String nextDistributedKey;
     private final Position position;
-    private boolean isLast;
+    private final boolean isLast;
     @Nullable
     private final Map<String, String> renaming;
     //TODO: make immutable
@@ -57,6 +33,8 @@ public class Node {
 
         CheckerUtils.checkNullOrEmpty(selectConditions, "selectConditions");
         this.selectConditions = selectConditions;
+
+        this.isLast = isLast;
     }
 
 }
