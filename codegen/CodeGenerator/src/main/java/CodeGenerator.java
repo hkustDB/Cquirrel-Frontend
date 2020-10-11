e97f72381bfa471c5f977ec480e3bd47959a1653
Danish Alsayed <danish.alsayed@gmail.com>
2020-10-11 18:16:32 +0800

    GCG-21 added compilation mechanism

--- codegen/CodeGenerator/src/main/java/CodeGenerator.java
+++ codegen/CodeGenerator/src/main/java/CodeGenerator.java
@@ -1,30 +1,71 @@
-import scala.collection.JavaConversions;
-import scala.tools.nsc.Global;
-import scala.tools.nsc.Settings;
-
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
-import static java.util.Collections.singletonList;
 import static java.util.Objects.requireNonNull;
 
 public class CodeGenerator {
+    private static final String GENERATED_CODE = "generated-code";
+
     public static void generate(Node node, String outputPath) throws IOException {
         requireNonNull(node);
         CheckerUtils.checkNullOrEmpty(outputPath, "outputPath");
-        new RelationProcessFunctionWriter(node.getRelationProcessFunction()).write(outputPath);
-        new AggregateProcessFunctionWriter(node.getAggregateProcessFunction()).write(outputPath);
-        String mainClassName = new MainClassWriter(node).write(outputPath);
 
-        compile(outputPath + File.separator + mainClassName + ".scala");
+        String rpfClassName = new RelationProcessFunctionWriter(node.getRelationProcessFunction()).write(outputPath);
+        copyToGeneratedCode(outputPath, rpfClassName);
+
+        String agpClassName = new AggregateProcessFunctionWriter(node.getAggregateProcessFunction()).write(outputPath);
+        copyToGeneratedCode(outputPath, agpClassName);
+
+        String mainClassName = new MainClassWriter(node).write(outputPath);
+        copyToGeneratedCode(outputPath, mainClassName);
+
+        compile(outputPath + File.separator + GENERATED_CODE + File.separator + "pom.xml");
+
     }
 
-    private static void compile(String filePath) {
-        Global global = new Global(new Settings());
-        Global.Run run = global.new Run();
-        List<String> fileNames = new ArrayList<>(singletonList(filePath));
-        run.compile(JavaConversions.asScalaBuffer(fileNames).toList());
+    private static void copyToGeneratedCode(String outputPath, String className) throws IOException {
+        Files.copy(
+                Paths.get(getClassFilePath(outputPath, className)),
+                Paths.get(outputPath + File.separator + GENERATED_CODE + "/src/main/scala" + File.separator + getClassFileName(className)),
+                StandardCopyOption.REPLACE_EXISTING
+        );
+    }
+
+    private static void compile(String pomPath) throws IOException {
+        Runtime runtime = Runtime.getRuntime();
+        execute(runtime, "mvn compile -f " + pomPath);
+        execute(runtime, "mvn package -f " + pomPath);
+    }
+
+    private static void execute(Runtime runtime, String command) throws IOException {
+        Process process = runtime.exec(command);
+
+        BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+        BufferedReader stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+
+        System.out.println("Running " + command + ":\n");
+        String output = null;
+        while ((output = stdInput.readLine()) != null) {
+            System.out.println(output);
+        }
+
+        System.out.println("Errors of " + command + " (if any):\n");
+        while ((output = stdError.readLine()) != null) {
+            System.out.println(output);
+        }
+    }
+
+    private static String getClassFilePath(String outputPath, String className) {
+        return outputPath + File.separator + getClassFileName(className);
+    }
+
+    private static String getClassFileName(String name) {
+        return name + ".scala";
     }
 }
---
+++ codegen/CodeGenerator/src/test/resources/generated-code/src/main/scala/Job.scala
@@ -0,0 +1,53 @@
+import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.core.fs.FileSystem
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.scala._
+import org.hkust.ProcessFunction.Q6.{Q6AggregateProcessFunction, Q6lineitemProcessFunction}
+import org.hkust.RelationType.Payload
+object Job {
+   def main(args: Array[String]) {
+      val env = StreamExecutionEnvironment.getExecutionEnvironment
+      val params: ParameterTool = ParameterTool.fromArgs(args)
+      env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+      var executionConfig = env.getConfig
+      executionConfig.enableObjectReuse()
+      val inputpath = "/Users/tom/lineitem.tbl"
+      val outputpath = "/Users/tom/AggrQ6output.csv"
+      val inputStream : DataStream[Payload] = getStream(env,inputpath)
+      val result  = inputStream.keyBy(i => i._3)
+      .process(new Q6lineitemProcessFunction())
+      .keyBy(i => i._3)
+      .process(new Q6AggregateProcessFunction)
+      .map(x => (x._4.mkString(", "), x._5.mkString(", ")))
+      .writeAsText(outputpath,FileSystem.WriteMode.OVERWRITE)
+      .setParallelism(1)
+      env.execute("Flink Streaming Scala API Skeleton")
+   }
+   private def getStream(env: StreamExecutionEnvironment, dataPath: String): DataStream[Payload] = {
+      val data = env.readTextFile(dataPath).setParallelism(1)
+      val format = new java.text.SimpleDateFormat("yyyy-MM-dd")
+      var cnt : Long = 0
+      val restDS : DataStream[Payload] = data
+      .map(line => {
+      val header = line.substring(0,3)
+      val cells : Array[String] = line.substring(3).split("\\|")
+      val i = Tuple4(cells(6).toDouble,format.parse(cells(10)),cells(5).toDouble,cells(4).toDouble)
+      var relation = ""
+      var action = ""
+      header match {
+      case "+LI" =>
+      relation = "Lineitem"
+      action = "Insert"
+      case "-LI" =>
+      relation = "Lineitem"
+      action = "Delete"
+      }
+      cnt = cnt + 1
+      Payload(relation, action,
+      Tuple2(cells(0).toInt, cells(3).toInt).asInstanceOf[Any],
+      Array(i._1,i._2,i._3,i._4),
+      Array("L_DISCOUNT","L_SHIPDATE","L_EXTENDEDPRICE","L_QUANTITY"), cnt)
+      }).setParallelism(1).filter(x => x._1 != "").setParallelism(1)
+      restDS
+   }
+   }
