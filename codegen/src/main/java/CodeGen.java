193c0ef7519b8fa4889ec4f5c82e1a322cd8aa31
Danish Alsayed <danish.alsayed@gmail.com>
2020-10-13 23:00:29 +0800

    GCG-24 made config for building a runnable jar

--- codegen/src/main/java/CodeGen.java
+++ codegen/src/main/java/CodeGen.java
@@ -5,15 +5,15 @@ import java.util.Arrays;
 import java.util.Set;
 
 public class CodeGen {
-    private int NUM_OF_ARGS = 5;
-    private final Set<String> IO_TYPES = ImmutableSet.of("file", "kafka");
-    private final int JSON_FILE_INDEX = 0;
-    private final int GENERATED_JAR_INDEX = 1;
-    private final int OUTPUT_PATH_INDEX = 2;
-    private final int INPUT_PATH_INDEX = 3;
-    private final int IO_TYPE_INDEX = 4;
+    private static int NUM_OF_ARGS = 5;
+    private static final Set<String> IO_TYPES = ImmutableSet.of("file", "kafka");
+    private static final int JSON_FILE_INDEX = 0;
+    private static final int GENERATED_JAR_INDEX = 1;
+    private static final int OUTPUT_PATH_INDEX = 2;
+    private static final int INPUT_PATH_INDEX = 3;
+    private static final int IO_TYPE_INDEX = 4;
 
-    public void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         System.out.println("\n" +
                 "   _     __                                 _        ___           \n" +
                 "  /_\\    \\ \\  /\\ /\\            ___ ___   __| | ___  / _ \\___ _ __  \n" +
@@ -26,7 +26,7 @@ public class CodeGen {
         CodeGenerator.generate(args[JSON_FILE_INDEX], args[GENERATED_JAR_INDEX], args[INPUT_PATH_INDEX], args[OUTPUT_PATH_INDEX]);
     }
 
-    private void validateArgs(String[] args) {
+    private static void validateArgs(String[] args) {
         if (args.length != NUM_OF_ARGS) {
             throw new RuntimeException("Expecting exactly " + NUM_OF_ARGS + " input strings: JSON file path, generated-code jar output path, flink input path, flink output path and flink I/O type");
         }
@@ -38,7 +38,7 @@ public class CodeGen {
         validateFlinkIOType(args[IO_TYPE_INDEX]);
     }
 
-    private void validateJsonFile(String jsonFilePath) {
+    private static void validateJsonFile(String jsonFilePath) {
         CheckerUtils.checkNullOrEmpty(jsonFilePath, "jsonFilePath");
         if (jsonFilePath.endsWith(".json")) {
             if (new File(jsonFilePath).exists()) return;
@@ -48,7 +48,7 @@ public class CodeGen {
         }
     }
 
-    private void validateDirectoryPath(String directoryPath) {
+    private static void validateDirectoryPath(String directoryPath) {
         CheckerUtils.checkNullOrEmpty(directoryPath, "directoryPath");
         File outputDir = new File(directoryPath);
         if (outputDir.exists() && outputDir.isDirectory()) return;
@@ -56,7 +56,7 @@ public class CodeGen {
         throw new RuntimeException("output directory must exist and must be a directory, got: " + directoryPath);
     }
 
-    private void validateFlinkIOType(String ioType) {
+    private static void validateFlinkIOType(String ioType) {
         CheckerUtils.checkNullOrEmpty(ioType, "ioType");
         if (!IO_TYPES.contains(ioType.toLowerCase())) {
             throw new RuntimeException("Only the following flink IO types are supported: " + Arrays.toString(IO_TYPES.toArray()));
