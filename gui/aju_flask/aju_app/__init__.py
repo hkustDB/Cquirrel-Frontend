2bfb37c44d219bf624033e8dfd1221742392c9b6
prncoprs <prncoprs@163.com>
2020-10-15 19:34:08 +0800

    GCG-19 adjust the output view

--- gui/aju_flask/aju_app/__init__.py
+++ gui/aju_flask/aju_app/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_socketio import SocketIO
+import time
+from threading import Lock
+import psutil
+from confluent_kafka import Consumer
 
 from config import config_options
 
 bootstrap = Bootstrap()
 socketio = SocketIO()
+thread = None
+thread_lock = Lock()
 
 
 def create_app(config_name):
@@ -20,3 +26,48 @@ def create_app(config_name):
     app.register_blueprint(main_blueprint)
 
     return app
+
+
+@socketio.on('connect')
+def send_kafka_data():
+    global thread
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(target=background_send_kafka_data_thread)
+
+
+def background_send_kafka_data_thread():
+    with open("/Users/chaoqi/Projects/AJU/code/gui-codegen/gui/aju_flask/aju_app/resources/AggrQ3output.csv", 'r') as f:
+        while True:
+            time.sleep(1)
+            line = f.readline()
+            if line:
+                line_list = line.replace('\n', '').replace('\r', '').split(',')
+                print("send: ", str(line_list))
+                socketio.emit('result_figure_data', {'data': line_list})
+            else:
+                f.seek(0)
+
+
+def background_send_kafka_data_thread_real():
+    KAFKA_HOME_PATH = '/Users/chaoqi/Programs/kafka_2.12-2.6.0'
+    KAFKA_CONSUMER_BOOTSTRAP_SERVERS = 'localhost:9092'
+    KAFKA_CONSUMER_GROUP_ID = 'aju_generated_jar_output_group'
+    KAFKA_CONSUMER_TOPIC = 'aju_generated_jar_output'
+    KAFKA_CONSUMER_AUTO_OFFSET_RESET = 'earliest'
+
+    kafka_consumer = Consumer({
+        'bootstrap.servers': KAFKA_CONSUMER_BOOTSTRAP_SERVERS,
+        'group.id': KAFKA_CONSUMER_GROUP_ID,
+        'auto.offset.reset': KAFKA_CONSUMER_AUTO_OFFSET_RESET
+    })
+    kafka_consumer.subscribe([KAFKA_CONSUMER_TOPIC])
+
+    while True:
+        time.sleep(1)
+        msg = kafka_consumer.poll(1)
+        if msg:
+            print(msg)
+            msg_list = msg.value().decode('utf-8').replace('\n', '').replace('\r', '').split(',')
+            print("send: ", str(msg_list))
+            socketio.emit('result_figure_data', {'data': msg_list})
