3a8e8b17adbf5f015961a34c904f3140de7f71fa
prncoprs <prncoprs@163.com>
2020-11-27 18:37:58 +0800

    GCG-25 remove unnecessary code

--- gui/aju_django/main/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-
-class UploadJsonFileForm(forms.Form):
-    uploaded_json_file = forms.FileField()
--- gui/aju_django/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('download_codegen_log', views.download_codegen_log, name='download_codegen_log'),
-    path('download_generated_jar', views.download_generated_jar, name='download_generated_jar'),
-]
