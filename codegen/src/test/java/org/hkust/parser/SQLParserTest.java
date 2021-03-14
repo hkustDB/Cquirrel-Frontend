9a3cda580378742bfea23ca09c2b8878583ac4b6
prncoprs <prncoprs@163.com>
2021-03-15 00:59:50 +0800

    GCG-51 init the test for sqlparser

--- codegen/src/test/java/org/hkust/parser/SQLParserTest.java
+++ codegen/src/test/java/org/hkust/parser/SQLParserTest.java
@@ -1,8 +1,13 @@
 package org.hkust.parser;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SQLParserTest {
@@ -32,8 +37,184 @@ public class SQLParserTest {
                 "o_orderdate, \n" +
                 "o_shippriority;";
 
+        String expected = "{\n" +
+                "  \"AggregateProcessFunction\": [\n" +
+                "    {\n" +
+                "      \"this_key\": [\n" +
+                "        \"orderkey\",\n" +
+                "        \"o_orderdate\",\n" +
+                "        \"o_shippriority\"\n" +
+                "      ],\n" +
+                "      \"AggregateValue\": [\n" +
+                "        {\n" +
+                "          \"value_type\": \"double\",\n" +
+                "          \"name\": \"revenue\",\n" +
+                "          \"aggregation\": \"+\",\n" +
+                "          \"value\": {\n" +
+                "            \"right_field\": {\n" +
+                "              \"right_field\": {\n" +
+                "                \"name\": \"l_discount\",\n" +
+                "                \"type\": \"attribute\",\n" +
+                "                \"relation\": \"lineitem\"\n" +
+                "              },\n" +
+                "              \"left_field\": {\n" +
+                "                \"var_type\": \"int\",\n" +
+                "                \"type\": \"constant\",\n" +
+                "                \"value\": 1\n" +
+                "              },\n" +
+                "              \"type\": \"expression\",\n" +
+                "              \"operator\": \"-\"\n" +
+                "            },\n" +
+                "            \"left_field\": {\n" +
+                "              \"name\": \"l_extendedprice\",\n" +
+                "              \"type\": \"attribute\",\n" +
+                "              \"relation\": \"lineitem\"\n" +
+                "            },\n" +
+                "            \"type\": \"expression\",\n" +
+                "            \"operator\": \"*\"\n" +
+                "          }\n" +
+                "        }\n" +
+                "      ],\n" +
+                "      \"output_key\": [\n" +
+                "        \"orderkey\",\n" +
+                "        \"o_orderdate\",\n" +
+                "        \"o_shippriority\"\n" +
+                "      ],\n" +
+                "      \"name\": \"QAggregate0\",\n" +
+                "      \"delta_output\": true\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"join_structure\": [\n" +
+                "    {\n" +
+                "      \"primary\": \"orders\",\n" +
+                "      \"foreign\": \"lineitem\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"primary\": \"customer\",\n" +
+                "      \"foreign\": \"orders\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"RelationProcessFunction\": [\n" +
+                "    {\n" +
+                "      \"this_key\": [\n" +
+                "        \"orderkey\"\n" +
+                "      ],\n" +
+                "      \"is_Last\": true,\n" +
+                "      \"select_conditions\": {\n" +
+                "        \"values\": [\n" +
+                "          {\n" +
+                "            \"right_field\": {\n" +
+                "              \"var_type\": \"Date\",\n" +
+                "              \"type\": \"constant\",\n" +
+                "              \"value\": \"1995-03-15\"\n" +
+                "            },\n" +
+                "            \"left_field\": {\n" +
+                "              \"name\": \"l_shipdate\",\n" +
+                "              \"type\": \"attribute\",\n" +
+                "              \"relation\": \"lineitem\"\n" +
+                "            },\n" +
+                "            \"type\": \"expression\",\n" +
+                "            \"operator\": \">\"\n" +
+                "          },\n" +
+                "          {\n" +
+                "            \"right_field\": {\n" +
+                "              \"name\": \"l_commitdate\",\n" +
+                "              \"type\": \"attribute\",\n" +
+                "              \"relation\": \"lineitem\"\n" +
+                "            },\n" +
+                "            \"left_field\": {\n" +
+                "              \"name\": \"l_receiptdate\",\n" +
+                "              \"type\": \"attribute\",\n" +
+                "              \"relation\": \"lineitem\"\n" +
+                "            },\n" +
+                "            \"type\": \"expression\",\n" +
+                "            \"operator\": \">\"\n" +
+                "          }\n" +
+                "        ],\n" +
+                "        \"operator\": \"&&\"\n" +
+                "      },\n" +
+                "      \"name\": \"Qlineitem\",\n" +
+                "      \"next_key\": [\n" +
+                "        \"orderkey\",\n" +
+                "        \"o_orderdate\",\n" +
+                "        \"o_shippriority\"\n" +
+                "      ],\n" +
+                "      \"is_Root\": true,\n" +
+                "      \"child_nodes\": 1,\n" +
+                "      \"relation\": \"lineitem\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"this_key\": [\n" +
+                "        \"custkey\"\n" +
+                "      ],\n" +
+                "      \"is_Last\": true,\n" +
+                "      \"select_conditions\": {\n" +
+                "        \"values\": [\n" +
+                "          {\n" +
+                "            \"right_field\": {\n" +
+                "              \"var_type\": \"Date\",\n" +
+                "              \"type\": \"constant\",\n" +
+                "              \"value\": \"1995-03-15\"\n" +
+                "            },\n" +
+                "            \"left_field\": {\n" +
+                "              \"name\": \"o_orderdate\",\n" +
+                "              \"type\": \"attribute\",\n" +
+                "              \"relation\": \"orders\"\n" +
+                "            },\n" +
+                "            \"type\": \"expression\",\n" +
+                "            \"operator\": \"<\"\n" +
+                "          }\n" +
+                "        ],\n" +
+                "        \"operator\": \"&&\"\n" +
+                "      },\n" +
+                "      \"name\": \"Qorders\",\n" +
+                "      \"next_key\": [\n" +
+                "        \"orderkey\"\n" +
+                "      ],\n" +
+                "      \"is_Root\": false,\n" +
+                "      \"child_nodes\": 1,\n" +
+                "      \"relation\": \"orders\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"this_key\": [\n" +
+                "        \"custkey\"\n" +
+                "      ],\n" +
+                "      \"is_Last\": true,\n" +
+                "      \"select_conditions\": {\n" +
+                "        \"values\": [\n" +
+                "          {\n" +
+                "            \"right_field\": {\n" +
+                "              \"var_type\": \"varchar\",\n" +
+                "              \"type\": \"constant\",\n" +
+                "              \"value\": \"BUILDING\"\n" +
+                "            },\n" +
+                "            \"left_field\": {\n" +
+                "              \"name\": \"c_mktsegment\",\n" +
+                "              \"type\": \"attribute\",\n" +
+                "              \"relation\": \"customer\"\n" +
+                "            },\n" +
+                "            \"type\": \"expression\",\n" +
+                "            \"operator\": \"==\"\n" +
+                "          }\n" +
+                "        ],\n" +
+                "        \"operator\": \"&&\"\n" +
+                "      },\n" +
+                "      \"name\": \"Qcustomer\",\n" +
+                "      \"next_key\": [\n" +
+                "        \"custkey\"\n" +
+                "      ],\n" +
+                "      \"is_Root\": false,\n" +
+                "      \"child_nodes\": 0,\n" +
+                "      \"relation\": \"customer\"\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
+
         SQLParser = new Parser(sql, "test.json");
 
         SQLParser.parse();
+
+        String actual= FileUtils.readFileToString(new File("test.json"),"UTF-8");
+        assertEquals(expected, actual);
     }
 }
