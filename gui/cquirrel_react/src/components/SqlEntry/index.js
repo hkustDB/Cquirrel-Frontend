4a12379f4dac3a82b3faa862d6a6b37ea8eaef98
prncoprs <prncoprs@163.com>
2021-03-22 00:08:50 +0800

    GCG-56 updates the sql query templates

--- gui/cquirrel_react/src/components/SqlEntry/index.js
+++ gui/cquirrel_react/src/components/SqlEntry/index.js
@@ -37,7 +37,7 @@ export default class SqlEntry extends Component {
                 "    avg(l_discount) as avg_disc, \n" +
                 "    count(*) as count_order\n" +
                 "from \n" +
-                "    lineitem\n" +
+                "    lineitem \n" +
                 "where \n" +
                 "    l_shipdate <= date '1998-09-02' \n" +
                 "group by \n" +
@@ -74,13 +74,13 @@ export default class SqlEntry extends Component {
         this.setState({
             code: "select \n" +
                 "    o_orderpriority, \n" +
-                "    count(distinct(orderkey))\n" +
+                "    count(distinct(l_orderkey)) as order_count\n" +
                 "from \n" +
                 "    lineitem, \n" +
                 "    orders\n" +
                 "where \n" +
-                "    o_orderdate >= 1993-07-01\n" +
-                "    and o_orderdate < 1993-10-01\n" +
+                "    o_orderdate >= date '1993-07-01'\n" +
+                "    and o_orderdate < date '1993-10-01'\n" +
                 "    and l_commitdate < l_recriptdate\n" +
                 "    and l_orderkey = o_orderkey\n" +
                 "group by \n" +
@@ -254,6 +254,115 @@ export default class SqlEntry extends Component {
         })
     }
 
+    setEditorCodeAsQ14 = () => {
+        this.setState({
+            code: "select\n" +
+                "    100.00 * sum(case \n" +
+                "        when p_type like 'PROMO%'\n" +
+                "        then l_extendedprice*(1-l_discount)\n" +
+                "        else 0\n" +
+                "    end) / sum(l_extendedprice * (1 - l_discount)) as promo_revenue\n" +
+                "from \n" +
+                "    lineitem, \n" +
+                "    part\n" +
+                "where \n" +
+                "    l_partkey = p_partkey\n" +
+                "    and l_shipdate >= date '1995-09-01'\n" +
+                "    and l_shipdate < date '1995-10-01';"
+        })
+    }
+
+    setEditorCodeAsQ16 = () => {
+        this.setState({
+            code: "select\n" +
+                "    p_brand, \n" +
+                "    p_type, \n" +
+                "    p_size, \n" +
+                "    count(distinct ps_suppkey) as supplier_cnt\n" +
+                "from \n" +
+                "    partsupp, \n" +
+                "    part,\n" +
+                "    supplier\n" +
+                "where \n" +
+                "    p_partkey = ps_partkey\n" +
+                "    and p_brand <> 'Brand#45'\n" +
+                "    and p_type not like 'MEDIUM POLISHED%'\n" +
+                "    and p_size in (49, 14, 23, 45, 19, 3, 36, 9)\n" +
+                "    and ps_suppkey = s_suppkey\n" +
+                "    and s_comment not like '%Customer%Complaints%'\n" +
+                "group by \n" +
+                "    p_brand, \n" +
+                "    p_type, \n" +
+                "    p_size;"
+        })
+    }
+
+    setEditorCodeAsQ18 = () => {
+        this.setState({
+            code: "select \n" +
+                "    * \n" +
+                "from (\n" +
+                "    select \n" +
+                "        c_name,\n" +
+                "        c_custkey, \n" +
+                "        o_orderkey,\n" +
+                "        o_orderdate,\n" +
+                "        o_totalprice,\n" +
+                "        sum(l_quantity) as CNT\n" +
+                "    from \n" +
+                "        customer,\n" +
+                "        orders,\n" +
+                "        lineitem\n" +
+                "    where \n" +
+                "        c_custkey = o_custkey\n" +
+                "        and o_orderkey = l_orderkey\n" +
+                "    group by \n" +
+                "        c_name, \n" +
+                "        c_custkey, \n" +
+                "        o_orderkey, \n" +
+                "        o_orderdate, \n" +
+                "        o_totalprice) as T\n" +
+                "where \n" +
+                "    CNT > 300;"
+        })
+    }
+
+    setEditorCodeAsQ19 = () => {
+        this.setState({
+            code: "select\n" +
+                "    sum(l_extendedprice * (1 - l_discount) ) as revenue\n" +
+                "from \n" +
+                "    lineitem, \n" +
+                "    part\n" +
+                "where\n" +
+                "    p_partkey = l_partkey and \n" +
+                "    (\n" +
+                "        (\n" +
+                "            p_brand = 'Brand#12'\n" +
+                "            and p_container in ( 'SM CASE', 'SM BOX', 'SM PACK', 'SM PKG') \n" +
+                "            and l_quantity >= 1 and l_quantity <= 11\n" +
+                "            and p_size >= 1 and p_size <= 5\n" +
+                "        )\n" +
+                "        or \n" +
+                "        (\n" +
+                "            p_brand = 'Brand#23'\n" +
+                "            and p_container in ('MED BAG', 'MED BOX', 'MED PKG', 'MED PACK')\n" +
+                "            and l_quantity >= 10 and l_quantity <= 20\n" +
+                "            and p_size >= 1 and p_size <= 10\n" +
+                "        )\n" +
+                "        or \n" +
+                "        (\n" +
+                "            p_brand = 'Brand#34'\n" +
+                "            and p_container in ( 'LG CASE', 'LG BOX', 'LG PACK', 'LG PKG')\n" +
+                "            and l_quantity >= 20 and l_quantity <= 30\n" +
+                "            and p_size >= 1 and p_size <= 15\n" +
+                "        )\n" +
+                "    )\n" +
+                "    and l_shipmode in ('AIR', 'AIR REG')\n" +
+                "    and l_shipinstruct = 'DELIVER IN PERSON';"
+        })
+    }
+
     setEditorCodeAsNull = () => {
         this.setState({
             code: ""
@@ -308,6 +417,10 @@ export default class SqlEntry extends Component {
                         <Button shape="round" onClick={this.setEditorCodeAsQ9}>Q9</Button>
                         <Button shape="round" onClick={this.setEditorCodeAsQ10}>Q10</Button>
                         <Button shape="round" onClick={this.setEditorCodeAsQ12}>Q12</Button>
+                        <Button shape="round" onClick={this.setEditorCodeAsQ14}>Q14</Button>
+                        <Button shape="round" onClick={this.setEditorCodeAsQ16}>Q16</Button>
+                        <Button shape="round" onClick={this.setEditorCodeAsQ18}>Q18</Button>
+                        <Button shape="round" onClick={this.setEditorCodeAsQ19}>Q19</Button>
                         <Button shape="round" onClick={this.setEditorCodeAsNull}>Clear</Button>
                     </div>
                 </Card>
