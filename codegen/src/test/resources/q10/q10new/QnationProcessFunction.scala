e87ed945370d8c9d8b3d8290889b33194a2e91c7
Chaoqi Zhang <prncoprs@163.com>
2021-03-26 23:06:21 +0800

    Revert "GCG-56 upload the new version generated q10 scala code."
    This reverts commit affe02c190b7e328e560f3df7657b6e98ad27988.

--- codegen/src/test/resources/q10/q10new/QnationProcessFunction.scala
+++
@@ -1,9 +0,0 @@
-import scala.math.Ordered.orderingToOrdered
-import org.hkust.BasedProcessFunctions.RelationFKProcessFunction
-import org.hkust.RelationType.Payload
-import java.util.Date
-class QnationProcessFunction extends RelationFKProcessFunction[Any]("nation",Array("NATIONKEY"),Array("NATIONKEY"),false) {
-override def isValid(value: Payload): Boolean = {
-   true
-   }
-   }
